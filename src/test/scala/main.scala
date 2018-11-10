9487b4ef5132290c85c7ab968c48223cc96539da
stmtk1 <13043069@gakushuin.ac.jp>
2018-11-10 18:55:53 +0900

    [Green] LogicGenerator.bool_to_int test implement

--- src/test/scala/main.scala
+++ src/test/scala/main.scala
@@ -1,7 +1,59 @@
-import org.scalatest._
+import org.scalatest.FunSpec
 
-class MainTest extends FlatSpec with DiagrammedAssertions {
-  "Hello world" should "Hello wordl" in {
-    assert(true)
+class LogicGeneratorTest extends FunSpec {
+  //with DiagrammedAssertions {
+  describe("コンストラクタ") {
+    describe("引数") {
+      it("size"){
+        val lg : LogicGenerator = new LogicGenerator(3)
+        assert(lg.size == 3)
+      }
+    }
+  }
+  describe("bools_to_int"){ 
+    describe("長さ２"){
+      it("all true"){
+        assert(LogicGenerator.bools_to_int(Array(true, true)) == 3)
+      }
+      
+      it("(false, true)"){
+        assert(LogicGenerator.bools_to_int(Array(false, true)) == 1)
+      }
+      
+      it("all false"){
+        assert(LogicGenerator.bools_to_int(Array(false, false)) == 0)
+      }
+    }
+    describe("長さ３"){
+      it("all false"){
+        assert(LogicGenerator.bools_to_int(Array(false, false, false)) == 0)
+      }
+      
+      it("(false, false, true)"){
+        assert(LogicGenerator.bools_to_int(Array(false, false, true)) == 1)
+      }
+      
+      it("(true, false, false)"){
+        assert(LogicGenerator.bools_to_int(Array(true, false, false)) == 4)
+      }
+      
+      it("(false, true, false)"){
+        assert(LogicGenerator.bools_to_int(Array(false, true, false)) == 2)
+      }
+      
+      it("all true"){
+        assert(LogicGenerator.bools_to_int(Array(true, true, true)) == 7)
+      }
+    }
+    
+    describe("長さ４"){
+      it("all false"){
+        assert(LogicGenerator.bools_to_int(Array(false, false, false, false)) == 0)
+      }
+      
+      it("all true"){
+        assert(LogicGenerator.bools_to_int(Array(true, true, true, true)) == 15)
+      }
+    }
   }
 }
